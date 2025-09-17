d02357fd2702eaf5ef8c3c6296f92a80d7a7eb7c
Zammil00 <muzammilkw07@gmail.com>
2025-09-17 12:10:09 +0700

    Update Splash View

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,23 +1,19 @@
 import 'package:get/get.dart';
+import 'package:labil_video_editor/app/routes/app_pages.dart';
 
 class SplashController extends GetxController {
-  //TODO: Implement SplashController
-
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    // KITA AKTIFKAN FUNGTION NAVISA KETIKA INIT CONTROLERNYA
+    navigasi();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  void navigasi() async {
+    // DISINI LOGIKA NAVIGASI KE HOMENYA
+    await Future.delayed(Duration(seconds: 4));
 
-  @override
-  void onClose() {
-    super.onClose();
+    // LANGSUNG KE HOMENYA
+    Get.offAllNamed(Routes.HOME);
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 import '../controllers/splash_controller.dart';
 
@@ -8,15 +10,30 @@ class SplashView extends GetView<SplashController> {
   const SplashView({super.key});
   @override
   Widget build(BuildContext context) {
+    // KITA PASANG GET PUT NTUK OVVERIDE KE CONTROLERNYA
+    final SplashController controller = Get.put(SplashController());
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SplashView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              "assets/animation/computer_operator.json",
+              width: 350,
+              height: 350,
+            ),
+            SizedBox(height: 70),
+            Text(
+              "LABIL VIDEO EDITING",
+              style: GoogleFonts.sora(
+                color: Colors.black54,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
