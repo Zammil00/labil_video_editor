f09344f16de6a28cd5359a4c0c0668e4709d2063
Zammil00 <muzammilkw07@gmail.com>
2025-09-17 14:37:51 +0700

    Update Home View

--- lib/app/modules/editor/views/editor_view.dart
+++ lib/app/modules/editor/views/editor_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/editor_controller.dart';
 
@@ -10,13 +11,49 @@ class EditorView extends GetView<EditorController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('EditorView'),
+        backgroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          'Editor',
+          style: GoogleFonts.sora(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'EditorView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.blueGrey.shade900,
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Text(
+                  "Pilih Video Ntuk Mulai Edit",
+                  style: GoogleFonts.sora(
+                    color: Colors.white70,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 25),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                ),
+                child: Text(
+                  "Import Video",
+                  style: GoogleFonts.sora(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:labil_video_editor/app/routes/app_pages.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -10,13 +12,45 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        backgroundColor: Colors.black,
+        title: Text(
+          'LABIL EDITOR',
+          style: GoogleFonts.sora(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            Text(
+              "Selamat Membuat Karya Yang Menarik",
+              style: GoogleFonts.sora(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+              ),
+
+              onPressed: () {
+                Get.toNamed(Routes.EDITOR);
+              },
+              child: Text(
+                "New Poject",
+                style: GoogleFonts.sora(color: Colors.white),
+              ),
+            ),
+          ],
         ),
       ),
     );
